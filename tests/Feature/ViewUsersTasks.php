b0631d3a81381daf54df9191c4ef5c9b3a990af5
quimgc <quimgonzalez@iesebre.com>
2017-10-17 20:01:07 +0200

    Initial version

---
+++ tests/Feature/ViewUsersTasks.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Task;
+use App\User;
+use PhpParser\Builder\Use_;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ViewUsersTasks extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function an_user_can_see_is_own_tasks()
+    {
+
+        //Preparar test
+
+        $user = factory(User::class)->create();
+        $tasks = factory(Task::class,5) -> create();
+
+        $user->tasks()->saveMany($tasks);
+
+
+        //Executar codi
+
+        $response = $this->get('user/'.$user->id.'/tasks');
+
+        //Comprovacions
+
+        $response->assertSuccessful();
+        $response->assertViewIs('user_tasks');
+        $response->assertViewHas('tasks',$user->tasks);
+
+        $response->assertSeeText($user->name . 'Tasks: ');
+
+        foreach ($tasks as $task) {
+            $response->assertSeeText($task->name);
+        }
+
+
+//
+//        $response->assertSeeText($tasks[0]->name);
+//        $response->assertSeeText($tasks[1]->name);
+//        $response->assertSeeText($tasks[2]->name);
+//        $response->assertSeeText($tasks[3]->name);
+//        $response->assertSeeText($tasks[4]->name);
+//
+//        // return view['user_tasks'];
+
+
+
+    }
+}
